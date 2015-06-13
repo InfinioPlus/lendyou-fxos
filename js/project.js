c0c08eac1884fef08f184253443e25de0b99a955
GhostAlgorithm <gomezlopez.jorge96@gmail.com>
2015-06-13 17:19:42 +0200

    Adding javascript functionality for design

--- js/project.js
+++ js/project.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
     $('.input-group.date').datepicker({
     });
+    
+    $('#new-lend').hide();
+    
+    $('#new-lend-btn').click(function(){
+        $('#new-lend').show();
+    });
+    
+    $('#close-btn').click(function(){
+        $('#lendwhat-txt').val('');
+        $('#lendto-txt').val('');
+        $('#lendwhen-txt').val('');
+        $('#new-lend').hide();
+    });
 });
