be495e468803ac55e04ae2cebf447549dba0b3be
GhostAlgorithm <gomezlopez.jorge96@gmail.com>
2015-06-13 21:17:45 +0200

    Install process 1

---
+++ js/install.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    if('mozApps' in navigator) {
+        var manifest_url = location.href + 'manifest.webapp';
+        var installCheck = navigator.mozApps.checkInstalled(manifest_url);
+        
+        installCheck.onsuccess = function() {
+            alert("caimos por aqui");
+            if(installCheck.result) {
+                $('#install-row').hide();
+            } else {
+                $('#install-row').click(function(ev){
+                    ev.preventDefault();
+                    
+                    var installLocFind = navigator.mozApps.install(manifest_url);
+                    
+                    installLocFind.onsuccess = function(data) {};
+                    
+                    installLocFind.onerror = function() {
+                        alert(installLocFind.error.name);
+                    };
+                });
+            };
+        };
+    } else{
+        $('#install-row').hide();
+    }
+});
