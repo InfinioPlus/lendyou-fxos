5074c530c5879d68d92ba70d07a6f2fcf6010e26
GhostAlgorithm <gomezlopez.jorge96@gmail.com>
2015-06-17 22:19:21 +0200

    code cleanup (fixing a syntax error)

--- js/project.js
+++ js/project.js
@@ -2,19 +2,8 @@ $(document).ready(function(){
     var primaryKeys;	// Stores Lends 
     databaseExists();
     getLends();
-    
-    var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-]; 
 
-    var getContacts = function(str){
+    var getContacts = function(){
         return function findMatches(q, cb) {
             var matches, substringRegex;
             
@@ -25,7 +14,7 @@ $(document).ready(function(){
     }
 
     // check if it is Firefox OS
-    if (navigator.userAgent.indexOf('Firefox') > -1 && navigator.userAgent.indexOf("Mobile") > -1)){
+    if (navigator.userAgent.indexOf('Firefox') > -1 && navigator.userAgent.indexOf("Mobile") > -1){
         $('#lendto-txt').typeahead({
             hint: true,
             highlight: true,
@@ -33,7 +22,7 @@ $(document).ready(function(){
         },
         {
             name: 'states',
-            source: getContacts(states)
+            source: getContacts()
         }); 
     }
     
